0e3923f5337108598319465c9749f9ff67a81f67
TheChavi <josejdc070@gmail.com>
2023-10-18 08:04:28 -0600

    First version finished

--- BusinessLogic/ServerResourcesDAO.cs
+++ BusinessLogic/ServerResourcesDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace BusinessLogic
 {
     public class ServerResourcesDAO
     {
+        public List<Domain.ServerResources> GetServersResourcesList(Domain.ServerData server)
+        {
+            List<Domain.ServerResources> serverResourcesListFound = new List<Domain.ServerResources>();
+            using (var entities = new SNMPdbEntities())
+            {
+                List<DataAccess.ServerResources> serverResourcesListFind = new List<DataAccess.ServerResources>();
+                serverResourcesListFind = (from serverResources in entities.ServerResources
+                                           where serverResources.idServer == server.IdServer
+                                           select serverResources).ToList();
+                if (serverResourcesListFind.FirstOrDefault() != null)
+                {
+                    foreach (DataAccess.ServerResources serverResourceFound in serverResourcesListFind)
+                    {
+                        serverResourcesListFound.Add(new Domain.ServerResources(serverResourceFound));
+                    }
+                }
+            }
+            return serverResourcesListFound;
+        }
     }
 }
